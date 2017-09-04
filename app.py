2bc1ec174ca2ee73559ce27dc184c0b3d5b38899
EC2 Default User <ec2-user@ip-172-31-15-85.us-east-2.compute.internal>
2017-09-04 18:43:03 +0000

    Changes...

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ from api.v1 import webapi
 app = Flask(__name__)
 webapi.init_app(app)
 
-app.run(debug=False, host='0.0.0.0', port=80)
+if __name__ == "__main__":
+   app.run(debug=False, host='0.0.0.0', port=80)
