8f0bbc43edb1bcaa670b31d03dba8f8c309c63f2
Leonardo Carbone <leoanrdoz.carbone@gmail.com>
2017-09-05 23:46:23 -0300

    AWS resources

--- api/v1/aws.py
+++ api/v1/aws.py
@@ -1,8 +1,8 @@
 import boto3
 
 def running_instances():
-    ec2 = boto3.resource('ec2')
-    
+    ec2 = boto3.resource('ec2', region_name="us-east-2b")
+
     instances = ec2.instances.filter(
     Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
     for instance in instances:
